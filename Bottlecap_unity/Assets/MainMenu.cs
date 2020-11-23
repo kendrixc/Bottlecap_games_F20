4e87e38ef1e81a36762dfdb012855036ef5308c8
Anh Le <ale2539669@gmail.com>
2020-11-22 18:41:57 -0800

    Created the MainMenu
    Made MainMenu Scene
    Added Music + looping
    Added 3 different buttons + changing scenes to the specifed ones.
    Play -> go to arena Scene(can then later change)
    Setting -> just open the same meny to back
    Quit -> Application.Quit() pretty much

---
+++ Bottlecap_unity/Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start the game, go to the game scene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quit the Game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Changing to settings or go back to menu
+    public void GoToSettingMenu() 
+    {
+        SceneManager.LoadScene("SettingMenu");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
